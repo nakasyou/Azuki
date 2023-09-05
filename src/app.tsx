86adcb012d5b510c1ff500e1b28f1783e1731abb
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-05 21:30:52 +0900

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import { component$, useSignal } from '@builder.io/qwik'
 
 import qwikLogo from './assets/qwik.svg'
 import viteLogo from '/vite.svg'
-import './app.css'
 
 export const App = component$(() => {
   const count = useSignal(0)
