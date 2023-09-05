41bdc4b765436886055fecf67cc3602653245bc6
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-05 21:29:58 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import '@builder.io/qwik/qwikloader.js'
 
 import { render } from '@builder.io/qwik'
 import { App } from './app.tsx'
-import './index.css'
+import './main.css'
 
 render(document.getElementById('app') as HTMLElement, <App />)
