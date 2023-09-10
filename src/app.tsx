3916a4d05d51fcad55ed48a735406d79f24a760c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-10 16:52:11 +0900

    sdk

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,15 @@
 import { component$, useSignal } from '@builder.io/qwik'
+import * as sdk from "matrix-js-sdk";
 
 export const App = component$(() => {
   return (
     <>
       <div>Azuki</div>
       <p>Azuki is distributed super app.</p>
+      <button onClick$={() => {
+        console.log(sdk)
+        alert(sdk)
+      }}>xxx</button>
     </>
   )
 })
